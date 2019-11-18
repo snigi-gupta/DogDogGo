d880fd08731569579b4aed3693aa98e04e0bc06f
Anirudh Sridhar <as443@buffalo.edu>
2019-11-18 06:37:16 -0500

    #4 adding webpack config and package.json

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+var path = require('path');
+var webpack = require('webpack');
+var BundleTracker = require('webpack-bundle-tracker');
+
+module.exports = {
+	context: __dirname,
+	entry: './assets/js/index',
+	output: {
+		path: path.resolve('./political_search_engine/static/bundles/'),
+		filename: "[name]-[hash].js",
+	},
+	plugins: [
+		new BundleTracker({filename: './webpack-stats.json'}),
+	],
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: ['babel-loader']
+			}
+		]
+	},
+	resolve: {
+		extensions: ['*', '.js', '.jsx']
+	}
+};
