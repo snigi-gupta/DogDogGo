927396324d3fb257afaa062420fab2359cb51f58
Anirudh Sridhar <as443@buffalo.edu>
2019-11-27 17:06:16 -0500

    #7 updating the css + installing new packages

--- assets/js/searchresults.js
+++ assets/js/searchresults.js
@@ -4,8 +4,26 @@ import * as actionCreators from './actioncreators'
 import { bindActionCreators } from 'redux'
 import { actions } from './actioncreators'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faUser, faCheckCircle } from '@fortawesome/free-solid-svg-icons'
+import {
+	faUser,
+	faCheckCircle,
+	faMeh,
+	faSmileBeam,
+	faAngry,
+	faBook,
+	faReply,
+	faChartArea,
+	faChevronRight,
+	faNewspaper
+} from '@fortawesome/free-solid-svg-icons'
 import moment from 'moment'
+import ExternalImage from 'react-external-image'
+
+const sentimentEmoticonHash = {
+	positive: {icon: faSmileBeam, classname: 'sentiment-happy'},
+	negative: {icon: faAngry, classname: 'sentiment-angry'},
+	neutral: {icon: faMeh, classname: 'sentiment-neutral'}
+}
 
 class SearchResults extends React.Component {
 	constructor(props) {
@@ -23,9 +41,20 @@ class SearchResults extends React.Component {
 		const { actions } = this.props
 		actions.fetchTweets()
 			.then((res) => {
+				console.log(res)
 				let data = res.data
 				let tweets = data.map((tweet) => {
-					return {id: tweet.id, text: tweet.tweet_text, poi: tweet.poi_name, tweet_date: tweet.tweet_date, verified: tweet.verified}
+					return {
+						id: tweet.id,
+						text: tweet.tweet_text,
+						poi: tweet.poi_name,
+						tweet_date: tweet.tweet_date,
+						verified: tweet.verified,
+						sentiment: tweet.sentiment,
+						topic: tweet.topic,
+						impact: tweet.impact,
+						userProfileImage: 'https://pbs.twimg.com/profile_images/1097820307388334080/9ddg5F6v_normal.png'
+					}
 				})
 				this.setState({tweets: tweets, loading: false})
 			})
@@ -39,32 +68,57 @@ class SearchResults extends React.Component {
 			return <div key={`${tweet.id}-${i}`} className="tweet-card">
 				<div className="row">
 					<div className="col-md-1">
-						<FontAwesomeIcon className="align-right fa-2x" icon={faUser}/>
+						<ExternalImage
+							src={tweet.userProfileImage}
+							fallbackImages={['/static/img/twitter_placeholder.jpg']}
+							className="img-46 img-rounded"
+						/>
 					</div>
 					<div className="col-md-10 tweet-card-text">
 						<div className="row">
-							{tweet.poi}{'  '}
+							<span className="text-bold">{tweet.poi}</span>{'  '}
 							{ tweet.verified && 
-								<FontAwesomeIcon icon={faCheckCircle} className="verified-circle"/>
+							<FontAwesomeIcon icon={faCheckCircle} className="verified-circle"/>
 							}{'  '}
-							{moment(tweet.tweet_date).fromNow()}
+							<span className="text-grey">
+								{moment(tweet.tweet_date).fromNow()}
+							</span>
 						</div>
 						<div className="row">
 							{tweet.text}
 						</div>
+						<div className="row tweet-card-quicklinks">
+							<div className="col-md-3">
+								<FontAwesomeIcon
+									icon={sentimentEmoticonHash[tweet.sentiment]['icon']}
+									className={sentimentEmoticonHash[tweet.sentiment]['classname']}
+								/>
+								{' '}
+								<span>{tweet.sentiment}</span>
+							</div>
+							<div className="col-md-3">
+								<FontAwesomeIcon icon={faBook}/>
+								{' '}
+								<span>{tweet.topic}</span>
+							</div>
+							<div className="col-md-3">
+								<FontAwesomeIcon icon={faReply}/>
+								{' '}
+								{tweet['impact']['replies']} Replies
+							</div>
+							<div className="col-md-3">
+								<FontAwesomeIcon icon={faNewspaper}/>
+								{' '}
+								{tweet['impact']['articles']} Articles
+							</div>
+						</div>
+						<div className="row tweet-card-more-details">
+							Detailed Analysis
+							{'   '}
+							<FontAwesomeIcon icon={faChevronRight}/>
+						</div>
 					</div>
 					<div className="col-md-1">
-					</div>
-				</div>
-				<div className="row">
-					<div className="col-md-3">
-						<i className=""></i>
-					</div>
-					<div className="col-md-3">
-					</div>
-					<div className="col-md-3">
-					</div>
-					<div className="col-md-3">
 					</div>
 				</div>
 			</div>
--- search/views.py
+++ search/views.py
@@ -2,11 +2,18 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 import os
 import json
+import random
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+SENTIMENTS = ['positive', 'negative', 'neutral']
 
 # Create your views here.
 def index(request):
 	f = open(f'{BASE_DIR}/tweets/BarackObama.json')
 	data = json.loads(f.read())
+	data = data[:25]
+	for i in range(0, len(data)):
+		data[i]['sentiment'] = random.choice(SENTIMENTS)
+		data[i]['topic'] = 'dummy'
+		data[i]['impact'] = {'articles': random.randint(0, 50), 'replies': random.randint(0, 50)}
 	return JsonResponse(data, safe=False)
