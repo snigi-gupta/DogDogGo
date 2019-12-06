06f8f3f3f0275288c5ef93359002e23cae2278ce
Anirudh Sridhar <as443@buffalo.edu>
2019-12-05 19:17:50 -0500

    minor fixes

--- assets/js/poianalysis.js
+++ assets/js/poianalysis.js
@@ -4,40 +4,51 @@ import Plot from 'react-plotly.js'
 class POIAnalysis extends React.PureComponent {
 	render() {
 		const { analysis } = this.props
-		return <div>
+		return <div className="text-center">
 			<div className="row">
 				<div className="row">
-					<div className="col-md-6">
-						<Plot
-							key='source'
-							data={[
-							{
-							y: Object.values(analysis.hashtags),
-							x: Object.keys(analysis.hashtags),
-							type: 'bar',
-							},
-							]}
-							layout={{width: 480, height: 360, title: 'Distribution of Devices Used'}}
-						/>
-					</div>
-					<div className="col-md-6">
-						<Plot
-							key='sentiment'
-							data={[
-							{
-							values: Object.values(analysis.sentiment),
-							labels: Object.keys(analysis.sentiment),
-							type: 'pie',
-							marker: {color: 'red'},
-							},
-							]}
-							layout={{width: 480, height: 360, title: 'Sentiment Analysis'}}
-						/>
-					</div>
+					<Plot
+						key='sentiment'
+						data={[
+						{
+						values: Object.values(analysis.sentiment),
+						labels: Object.keys(analysis.sentiment),
+						type: 'pie',
+						marker: {color: 'red'},
+						},
+						]}
+						layout={{width: 480, height: 360, title: 'Sentiment Analysis'}}
+					/>
+				</div>
+				<div className="row">
+					<Plot
+						key='hashtags'
+						data={[
+						{
+						y: Object.values(analysis.hashtags),
+						x: Object.keys(analysis.hashtags),
+						type: 'bar',
+						},
+						]}
+						layout={{width: 480, height: 360, title: 'Distribution of Hashtags'}}
+					/>
+				</div>
+				<div className="row">
+					<Plot
+						key='source'
+						data={[
+						{
+						y: Object.values(analysis.source),
+						x: Object.keys(analysis.source),
+						type: 'bar',
+						},
+						]}
+						layout={{width: 480, height: 360, title: 'Distribution of Devices used by POI'}}
+					/>
 				</div>
 			</div>
 		</div>
-	}
+}
 }
 
 export default POIAnalysis
